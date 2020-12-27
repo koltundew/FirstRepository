754b41ddf7d6031fd28c1c2c7ca9daa3e2460bee
Kamil Szymański <kszym94@gmail.com>
2020-12-27 15:09:30 +0100

    Lambda

--- src/Library/app/LibraryControl.java
+++ src/Library/app/LibraryControl.java
@@ -136,27 +136,21 @@ public class LibraryControl {
     }
 
     private void printBooks() {
-        printer.printBooks(library.getSortedPublications(new AlphabeticalTitleComparator()));
+       printer.printBooks(library.getSortedPublications((p1,p2) ->
+               p1.getTitle().compareToIgnoreCase(p2.getTitle())));
     }
 
     private void printMagazines() {
-        printer.printMagazines(library.getSortedPublications(new AlphabeticalTitleComparator()));
+        printer.printMagazines(library.getSortedPublications((p1,p2) ->
+                p1.getTitle().compareToIgnoreCase(p2.getTitle())));
     }
 
     private void printUsers(){
-        printer.printUsers(library.getSortedUsers(new Comparator<LibraryUser>() {
-            @Override
-            public int compare(LibraryUser p1, LibraryUser p2) {
-                return p1.getLastName().compareToIgnoreCase(p2.getLastName());
-            }
-        }));
+        printer.printUsers(library.getSortedUsers((p1,p2 ) ->
+                p1.getLastName().compareToIgnoreCase(p2.getLastName())));
     }
 
-//    private Publication[] getSortedPublications(){
-//        Publication[] publications = library.getPublications();
-//        Arrays.sort(publications, new AlphabeticalTitleComparator());
-//        return publications;
-//    }
+
 
     private void deleteMagazine(){
         try{
