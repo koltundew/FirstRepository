c9ca87aea4d43698f1a83a1fc77e9e95c1f74ac3
Kamil Szymański <kszym94@gmail.com>
2021-01-05 20:16:49 +0100

    Kolekcje2

--- src/Library/io/ConsolePrinter.java
+++ src/Library/io/ConsolePrinter.java
@@ -1,45 +1,42 @@
 package Library.io;
-import Library.model.LibraryUser;
-import Library.model.Publication;
-import Library.model.Magazine;
-import Library.model.Book;
+import Library.model.*;
+
 import java.util.Collection;
 
 
 public class ConsolePrinter {
-    public void printBooks(Collection<Publication>publications){
-        int counter = 0;
-        for(Publication publication : publications){
-            if(publication instanceof Book){
-                printLine(publication.toString());
-                counter++;
-            }
-        }
-        if(counter == 0){
-            System.out.println("Brak książek w bibliotece");
-        }
-    }
-    public void printMagazines(Collection<Publication>publications){
-        int counter = 0;
-        for(Publication publication : publications){
-            if(publication instanceof Magazine){
-                printLine(publication.toString());
-                counter++;
-            }
-        }
-        if(counter == 0){
-            System.out.println("Brak książek w bibliotece");
-        }
-    }
 
-    public void printUsers(Collection<LibraryUser>users){
-        for(LibraryUser user : users){
-            printLine(user.toString());
-        }
+    public void printBooks(Collection<Publication> publications) {
+        long count = publications.stream()
+                .filter(p -> p instanceof Book)
+                .map(Publication::toString)
+                .peek(this::printLine)
+                .count();
+        if (count == 0)
+            printLine("Brak książek w bibliotece");
+
     }
 
 
-    public void printLine(String text){
-        System.out.println(text);
+    public void printMagazines(Collection<Publication> publications) {
+
+            long count = publications.stream()
+                    .filter(p -> p instanceof Magazine)
+                    .map(Publication::toString)
+                    .peek(this::printLine)
+                    .count();
+            if (count == 0)
+                printLine("Brak Magazynów w bibliotece");
+        }
+
+        public void printUsers (Collection < LibraryUser > users) {
+            users.stream()
+                    .map(User::toString)
+                    .forEach(this::printLine);
+        }
+
+
+        public void printLine (String text){
+            System.out.println(text);
+        }
     }
-}
